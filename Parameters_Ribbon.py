0fe95d3fc7500724e0908d1eb98ca7111cc8e550
APEbbers <paul.ebbers@gmail.com>
2024-08-16 17:33:15 +0200

    Fixed several issues with settings ToDO: Replace file and foder dialog. Issues with tkinter

--- Parameters_Ribbon.py
+++ Parameters_Ribbon.py
@@ -81,19 +81,16 @@ class Settings:
         return
 
     def SetBoolSetting(settingName: str, value):
-        if value.lower() == "true":
+        if str(value).lower() == "true":
             Bool = True
-        if str(value).lower() == "none" or value.lower() != "true":
+        if str(value).lower() == "none" or str(value).lower() != "true":
             Bool = False
         preferences.SetBool(settingName, Bool)
         return
 
     def SetIntSetting(settingName: str, value: int):
-        if value.lower() == "true":
-            Bool = True
-        if str(value).lower() == "none" or value.lower() != "true":
-            Bool = False
-        preferences.SetInt(settingName, Bool)
+        if str(value).lower() != "":
+            preferences.SetInt(settingName, value)
         return
 
     # endregion
@@ -105,20 +102,23 @@ STYLESHEET_LOCATION = os.path.dirname(__file__) + "/Resources/stylesheets/"
 UI_LOCATION = os.path.dirname(__file__) + "/Resources/ui/"
 
 # Define the icon sizes
-if Settings.GetIntSetting("IconSize_Small") is not None:
+if Settings.GetIntSetting("IconSize_Small") is not None or Settings.GetIntSetting("IconSize_Small") > 0:
     ICON_SIZE_SMALL = Settings.GetIntSetting("IconSize_Small")
 else:
     ICON_SIZE_SMALL = int(24)
+    Settings.SetIntSetting("IconSize_Small", 24)
 
-if Settings.GetIntSetting("IconSize_Medium") is not None:
+if Settings.GetIntSetting("IconSize_Medium") is not None or Settings.GetIntSetting("IconSize_Medium") > 0:
     ICON_SIZE_MEDIUM = Settings.GetIntSetting("IconSize_Medium")
+    Settings.SetIntSetting("IconSize_Medium", 44)
 else:
     ICON_SIZE_MEDIUM = int(44)
 
-if Settings.GetIntSetting("IconSize_Large") is not None:
+if Settings.GetIntSetting("IconSize_Large") is not None or Settings.GetIntSetting("IconSize_Large") > 0:
     ICON_SIZE_LARGE = Settings.GetIntSetting("IconSize_Large")
 else:
     ICON_SIZE_LARGE = int(64)
+    Settings.SetIntSetting("IconSize_Large", 64)
 
 
 # Backup parameters
@@ -126,17 +126,20 @@ if Settings.GetBoolSetting("BackupEnabled") is True:
     ENABLE_BACKUP = Settings.GetBoolSetting("BackupEnabled")
 else:
     ENABLE_BACKUP = bool(True)
-if Settings.GetBoolSetting("BackupEnabled") is True:
-    BACKUP_LOCATION = Settings.GetBoolSetting("BackupEnabled")
+
+if Settings.GetStringSetting("BackupFolder") != "":
+    BACKUP_LOCATION = Settings.GetStringSetting("BackupFolder")
 else:
     BACKUP_LOCATION = os.path.dirname(__file__) + "/Backups"
 
 # Additional parameter
 HELP_ADRESS = str("https://wiki.freecad.org/Main_Page")
+
 if Settings.GetBoolSetting("AutoHideRibbon") is True:
     AUTOHIDE_RIBBON = Settings.GetBoolSetting("AutoHideRibbon")
 else:
     AUTOHIDE_RIBBON = bool(False)
+
 if Settings.GetStringSetting("Stylesheet") != "":
     STYLESHEET = Settings.GetStringSetting("Stylesheet")
 else:
